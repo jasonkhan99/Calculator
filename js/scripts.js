3477f13eff12e622045dc7f2126552def8c7f374
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-01 15:17:49 -0700

    added second constant and used add function to add
    co-authored-by: Sean Downs<spdowns@protonmail.com>
    co-authored-by: Tre Sonna<tresonna@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ const add = function(number1, number2) {
   return number1 + number2;
 }
 
-alert(add(10, 5));
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+alert(add(number1, number2));
