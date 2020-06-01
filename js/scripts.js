e583f41ae04b15098a09593665cff507a93b720b
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-01 15:40:47 -0700

    add other functions for - * and /
    co-authored-by: Sean Downs<spdowns@protonmail.com>
    co-authored-by: Tre Sonna<tresonna@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 const add = function(number1, number2) {
   return number1 + number2;
-}
+};
 
+const subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+const division = function(number1, number2) {
+  return number1 / number2;
+};
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
-
-alert(add(number1, number2));
+const result = subtract(number1, number2);
+alert(result);
