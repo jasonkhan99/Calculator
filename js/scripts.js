603001c09636a0262da1dd12d7bbb3d86f43a8d3
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-03 14:11:21 -0700

    add document ready function to user logic
    Co-authored-by: Mook Dang <mookdang24@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,10 @@ const multiply = function(number1, number2) {
 const divide = function(number1, number2) {
   return number1 / number2;
 };
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = add(number1, number2);
-alert(result);
+
+$(document).ready(function() {
+  const number1 = parseInt(prompt("Enter a number:"));
+  const number2 = parseInt(prompt("Enter another number:"));
+
+  alert(add(number1, number2));
+});
